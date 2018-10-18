f3b597569ebc48aae9cfeaf9e7b26efeaab35c1f
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-18 14:22:20 -0700

    Car Insurance Approval qualifications

---
+++ Car_Insurance_Approval/Car_Insurance_Approval/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Applicant info input
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTix = Convert.ToInt16(Console.ReadLine());
+
+            //Evaluation
+            bool qualify = (age > 15 && (dui == "no"|| dui =="No") && speedingTix <= 3);
+            Console.WriteLine(qualify);
+            Console.Read();
+
+
+        }
+    }
+}
