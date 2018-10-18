ad2f8fd5729f844f267a42f187c2d3fa7ba35963
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-18 11:46:49 -0700

    Student Daily Report app

---
+++ Student_Daily_Report/Student_Daily_Report/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Student_Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool help = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you\'d like to provide? Please be specific.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hours = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+        }
+    }
+}
