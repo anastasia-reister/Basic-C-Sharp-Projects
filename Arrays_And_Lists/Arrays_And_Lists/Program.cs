5b3fbdae3659cbd66a59def80484d2328957d911
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-19 14:46:36 -0700

    Arrays and Lists Demo

---
+++ Arrays_And_Lists/Arrays_And_Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+        List<int> newList = new List<int>();
+        newList.Add(1);
+        newList.Add(2);
+
+        Console.WriteLine(newList[1]);
+        Console.Read();
+
+
+        //int[] numArray = new int[5]; //declaring an array of integer values of length 5; arrays are objects in c#
+        //numArray[0] = 100;
+        //numArray[1] = 200;
+        //numArray[2] = 300;
+        //numArray[3] = 400;
+        //numArray[4] = 500;
+
+        //int[] numArray1 = new int[] { 100, 200, 300, 400, 500 }; //inline declaration of array
+
+
+        //int[] numArray2 = { 100, 200, 300, 400, 500 }; //inline declaration of array
+    }
+    }
+
