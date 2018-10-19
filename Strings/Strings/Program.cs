745843bf2e8d47d1a33d917bff48eb1aeb30736e
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-19 14:46:47 -0700

    Strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -15,7 +15,7 @@ namespace Strings
             
 
             StringBuilder paragraph = new StringBuilder(50);
-            paragraph.AppendLine("There were a king with a large jaw and a queen with a plain face, on the throne of England");
+            paragraph.AppendLine("There were a king with a large jaw and a queen with a plain face, on the throne of England.");
             paragraph.AppendLine("There were a king with a large jaw and a queen with a fair face, on the throne of France.");
             paragraph.AppendLine("In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.");
             paragraph.AppendLine("It was the year of Our Lord one thousand seven hundred and seventy - five. ");
