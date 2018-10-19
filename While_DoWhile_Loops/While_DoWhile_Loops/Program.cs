73af8f495d6fd04ca18fef1aef401b798cd884ea
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-18 17:05:37 -0700

    While/ DoWhile Loops

---
+++ While_DoWhile_Loops/While_DoWhile_Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace While_DoWhile_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number lower than 25");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---");
+            int initNum = number;
+            while (number < 25)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            while (number >= initNum);
+
+            Console.Read();
+        }
+    }
+}
