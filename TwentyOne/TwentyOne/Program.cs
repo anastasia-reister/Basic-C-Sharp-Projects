54fb7e6b192696236d0555e8a55f3b40aeec9ff3
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 18:00:08 -0700

    add multiple shuffle

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,30 +11,39 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck(); //instantiate deck
-            deck = Shuffle(deck);
+            int timesShuffled = 0;
+            deck = Shuffle(deck, out timesShuffled, 3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shuffled " + timesShuffled);
             Console.Read();
         }
 
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
         {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while(deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count); //creates a random number between 0 and 52
-                TempList.Add(deck.Cards[randomIndex]); //moves an item from list deck.Cards to Templist at randomNumber index
-                deck.Cards.RemoveAt(randomIndex); //removes that item from Cards list
-            }
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
 
-            deck.Cards = TempList; //assignes all values of TempList to deck.Cards
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count); //creates a random number between 0 and 52
+                    TempList.Add(deck.Cards[randomIndex]); //moves an item from list deck.Cards to Templist at randomNumber index
+                    deck.Cards.RemoveAt(randomIndex); //removes that item from Cards list
+                }
+
+                deck.Cards = TempList; //assignes all values of TempList to deck.Cards
+            }
             return deck;
         }
+
+        
     }
 }
