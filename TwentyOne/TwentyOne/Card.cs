9f029dc079c2a98a4d6a4e3662eb2d707a63e9cf
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 17:08:36 -0700

    TwentyOne Game Demo

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card() //Constructor: default values assigned to properties and or/methods run upon instantiation of a class
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+
+    
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck(); //instantiate deck
+            deck = Shuffle(deck);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.Read();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while(deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count); //creates a random number between 0 and 52
+                TempList.Add(deck.Cards[randomIndex]); //moves an item from list deck.Cards to Templist at randomNumber index
+                deck.Cards.RemoveAt(randomIndex); //removes that item from Cards list
+            }
+
+            deck.Cards = TempList; //assignes all values of TempList to deck.Cards
+            return deck;
+        }
+    }
+}
