b0dfefca48a925d4ea6b4cfa99d0f107c76cdb11
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-23 11:33:07 -0700

    create class method

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -8,6 +8,7 @@ namespace TwentyOne
 {
     public class Deck
     {
+        //Constructor---------------------------------------
         public Deck()//construction name always same as name of class
         {
             Cards = new List<Card>(); //instantiating property Cards as an empty list
@@ -30,6 +31,27 @@ namespace TwentyOne
             }
 
         }
+
+        //Properties------------------------------------------
         public List<Card> Cards { get; set; } //property Cards  - list of type Card
+        
+        //Methods-------------------------------------------
+        public void Shuffle(int times = 1) //removed 'static' -- now method applies to its own class
+                                            //replaced Deck with void, because now doesn't need to return deck
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count); //creates a random number between 0 and 52
+                    TempList.Add(Cards[randomIndex]); //moves an item from list deck.Cards to Templist at randomNumber index
+                    Cards.RemoveAt(randomIndex); //removes that item from Cards list
+                }
+                Cards = TempList; //assignes all values of TempList to deck.Cards
+            }
+        }
     }
 }
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,38 +11,17 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck(); //instantiate deck
-            int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 3);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled " + timesShuffled);
             Console.Read();
         }
 
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count); //creates a random number between 0 and 52
-                    TempList.Add(deck.Cards[randomIndex]); //moves an item from list deck.Cards to Templist at randomNumber index
-                    deck.Cards.RemoveAt(randomIndex); //removes that item from Cards list
-                }
-
-                deck.Cards = TempList; //assignes all values of TempList to deck.Cards
-            }
-            return deck;
-        }
+       
 
         
     }
