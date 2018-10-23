a2659dcc2b53f5480f08386d9a9a216d5f0795d3
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 18:18:14 -0700

    Class_instantiating same name methods

---
+++ class_instantiating/class_instantiating/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_instantiating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            similar_methods method1 = new similar_methods(); //instantiating class similar_methods
+            // not sure if I had to instantiate the class all three times I called a method since they all belong to one class. So I only did once.
+            Console.WriteLine("Method that takes an int and returns an int and adds 5: " + method1.one_method(12));
+            
+            Console.WriteLine("Method that takes an decimal and returns an int and adds 10: " + method1.one_method(1.22m));
+
+            Console.WriteLine("Method that takes an string and returns an int and adds 20: " + method1.one_method("12"));
+
+            Console.Read();
+        }
+    }
+}
---
+++ class_instantiating/class_instantiating/similar_methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_instantiating
+{
+    class similar_methods
+    {
+        public int one_method( int num)
+        {
+            int result = num + 5;
+            return result;
+        }
+
+        public int one_method(decimal num)
+        {
+            int result = Convert.ToInt32(num) + 10;
+            return result;
+        }
+
+        public int one_method(string num)
+        {
+            int result = Convert.ToInt32(num) + 20;
+            return result;
+        }
+    }
+}
