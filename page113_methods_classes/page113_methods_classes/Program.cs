87b292fe3b5066e991696eb43893e01c14065757
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 17:59:48 -0700

    class instantiation and method calling

---
+++ page113_methods_classes/page113_methods_classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace page113_methods_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            moreMath math1 = new moreMath(); //instatiation of the class
+
+            moreMath.sampleMethod(first: 2, second: 3); //calling method; would not call method of instance since is a static method
+
+            Console.Read();
+
+            
+        }
+    }
+}
---
+++ page113_methods_classes/page113_methods_classes/moreMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page113_methods_classes
+{
+    public class moreMath
+    {
+        public static void sampleMethod(int first = 1, int second = 1)
+        {
+            int result = first * 25;
+            Console.WriteLine("Second number is: " + second);
+
+        }
+    }
+}
