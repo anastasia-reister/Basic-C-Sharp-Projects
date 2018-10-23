2d5b7706cab5383b7e63ba0e9f17aedb8d866340
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 18:03:49 -0700

    remove static

--- page113_methods_classes/page113_methods_classes/Program.cs
+++ page113_methods_classes/page113_methods_classes/Program.cs
@@ -8,7 +8,7 @@ namespace page113_methods_classes
         {
             moreMath math1 = new moreMath(); //instatiation of the class
 
-            moreMath.sampleMethod(first: 2, second: 3); //calling method; would not call method of instance since is a static method
+            math1.sampleMethod(first: 2, second: 3); //calling method; would not call method of instance since is a static method
 
             Console.Read();
 
--- page113_methods_classes/page113_methods_classes/moreMath.cs
+++ page113_methods_classes/page113_methods_classes/moreMath.cs
@@ -8,10 +8,10 @@ namespace page113_methods_classes
 {
     public class moreMath
     {
-        public static void sampleMethod(int first = 1, int second = 1)
+        public void sampleMethod(int first = 1, int second = 1) //method with 2 parameters
         {
-            int result = first * 25;
-            Console.WriteLine("Second number is: " + second);
+            int result = first * 25; //math
+            Console.WriteLine("Second number is: " + second);//display on screen
 
         }
     }
