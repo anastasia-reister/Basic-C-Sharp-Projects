04ec9e4a875d53a9ef5d42663dabac5bb2d7f2a8
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-22 17:08:10 -0700

    Calling Method Practice

---
+++ Class_Method_Practice/Class_Method_Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Class_Method_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int userNumber =  Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Here is what rounded 3/4 of our number is:");
+            Console.WriteLine(someMath.threeQuarters(userNumber));
+
+            Console.WriteLine("Here is waht rounded 150% of your number is:");
+            Console.WriteLine(someMath.percent150(userNumber));
+
+            Console.WriteLine("Here is how many quarters you can get from your number of dollars:");
+            Console.WriteLine(someMath.inQuarters(userNumber));
+
+            Console.Read();
+        }
+    }
+}
---
+++ Class_Method_Practice/Class_Method_Practice/someMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice
+{
+    public class someMath
+    {
+        public static float threeQuarters (int num)
+        {
+            float result = num * 3 / 4;
+            return result;
+        }
+
+        public static int percent150 (int num)
+        {
+            int result = num * 150 / 100;
+            return result;
+        }
+
+        public static int inQuarters (int dollars)
+        {
+            int quantity = Convert.ToInt32(dollars * 4);
+            return quantity;
+        }
+    }
+}
