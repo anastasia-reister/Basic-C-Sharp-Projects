11ad473f8aec192f84cff37ca3959c5fb18b0c32
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-24 16:59:47 -0700

    overload comparative operators == and != for employee object

--- Abstract_Person_Employee/Abstract_Person_Employee/Employee.cs
+++ Abstract_Person_Employee/Abstract_Person_Employee/Employee.cs
@@ -8,6 +8,7 @@ namespace Abstract_Person_Employee
 {
     class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
         public override void SayName()
         {
             Console.WriteLine("Name: [" + firstName + " " + lastName + "]");
@@ -17,5 +18,27 @@ namespace Abstract_Person_Employee
         {
             Console.WriteLine("I Quit!");
         }
+
+        public static bool operator==(Employee emp1, Employee emp2)
+        {
+            bool isEqual = false;
+            if(emp1.Id == emp2.Id)
+            {
+                isEqual = true;
+            }
+            return isEqual;
+        }
+
+        public static bool operator!=(Employee emp1, Employee emp2)
+        {
+            bool isNotEqual = false;
+            if (emp1.Id != emp2.Id)
+            {
+                 isNotEqual = true;
+            }
+            return isNotEqual;
+        }
+
+
     }
 }
--- Abstract_Person_Employee/Abstract_Person_Employee/Program.cs
+++ Abstract_Person_Employee/Abstract_Person_Employee/Program.cs
@@ -9,10 +9,28 @@ namespace Abstract_Person_Employee
         {
 
             Employee emp2 = new Employee();
-            emp2.firstName = "Sample";
-            emp2.lastName = "Student";
+            emp2.firstName = "Jim";
+            emp2.lastName = "Halpert";
+            emp2.Id = 222;
 
+            Employee emp1 = new Employee();
+            emp1.firstName = "James";
+            emp1.lastName = "Halpert";
+            emp1.Id = 111;
             emp2.SayName();
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine(emp1.firstName + " " + emp1.lastName + " and " + emp2.firstName
+                    + " " + emp2.lastName + " are indeed the same person!");
+            }
+            else if (emp1 != emp2)
+            {
+                Console.WriteLine(emp1.firstName + " " + emp1.lastName + " and " + emp2.firstName
+                    + " " + emp2.lastName + " are different people!");
+            }
+
+            Console.Read();
 
 
             IQuittable obj1 = new Employee(); //Not sure I understand. Does that mean obj1 is being instantiated from 
@@ -22,7 +40,7 @@ namespace Abstract_Person_Employee
             obj1.Quit();
 
 
-            Console.Read();
+            
         }
     }
 }
