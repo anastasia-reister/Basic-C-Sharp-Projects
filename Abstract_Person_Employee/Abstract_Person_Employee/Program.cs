a991c5603b13ce43c97143f4b182536a08fac14a
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-24 14:55:36 -0700

    create abstractclass and abstract method
    inherit  from abstract class, override abstract method

---
+++ Abstract_Person_Employee/Abstract_Person_Employee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Abstract_Person_Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp2 = new Employee();
+            emp2.firstName = "Sample";
+            emp2.lastName = "Student";
+
+            emp2.SayName();
+            Console.Read();
+        }
+    }
+}
