4a61be4f27e9c2605d732f03024b5143da6e8006
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-24 12:53:59 -0700

    Create Parent and child class
    Instantiate child class, call parent properties.

---
+++ Inheritance_Person_Employee/Inheritance_Person_Employee/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Person_Employee
+{
+    public class Employee : Person
+    {
+        //Properties --------
+        public int Id { get; set; }
+
+    }
+}
---
+++ Inheritance_Person_Employee/Inheritance_Person_Employee/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Person_Employee
+{
+    public class Person
+    {
+        //Properties -----------------
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: [" + FirstName + " " + LastName + "]");
+        }
+    }
+}
