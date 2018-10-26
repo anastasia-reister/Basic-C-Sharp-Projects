3ff5f63bb70796ae7195b7ae4dddc306b5a8b68b
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-26 12:03:12 -0700

    Create basic struct

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+            Numbers number = new Numbers();
+            number.Amount = 3.5m;
+            Console.WriteLine(number.Amount);
+            Console.Read();
+        }
+    }
+}
