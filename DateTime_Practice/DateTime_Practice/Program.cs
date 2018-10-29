aabbb692e32f898ba9310fb004917d3173e34019
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-29 13:19:38 -0700

    DateTime in X hours

---
+++ DateTime_Practice/DateTime_Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Enter a number of hours:");
+            TimeSpan Xhours = TimeSpan.FromHours(Convert.ToDouble(Console.ReadLine()));
+            DateTime inXHours = now + Xhours;
+            Console.WriteLine(inXHours);
+            Console.Read();
+        }
+    }
+}
