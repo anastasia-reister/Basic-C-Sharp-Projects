b5e62dc2c3c168e11abbf64ee8c8281fdf086c03
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-29 12:39:27 -0700

    create a log of entered numbers

---
+++ Log_Practice/Log_Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Log_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 5 numbers.");
+            int i = 0;
+            do
+            {
+                i++;
+                Console.WriteLine("Enter a number " + i + ": ");
+                string number = Console.ReadLine();
+                using (StreamWriter numberLog = new StreamWriter(@"C:\Users\Nastya\Documents\GitHub\Basic-C-Sharp-Projects\Log_Practice\numberLog.txt", true))
+                {
+                    numberLog.WriteLine(number);
+                }
+            }while( i < 5) ;
+
+            string numberLogString = File.ReadAllText(@"C:\Users\Nastya\Documents\GitHub\Basic-C-Sharp-Projects\Log_Practice\numberLog.txt");
+            Console.WriteLine("Numbers that have been entered: " + numberLogString);
+            Console.Read();
+        }
+    }
+}
