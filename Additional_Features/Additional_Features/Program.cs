6fa1f935a6fecf313ea2a2e16c9577ffba803fad
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-29 18:15:43 -0700

    Constant, var declaration, chained constructor

---
+++ Additional_Features/Additional_Features/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CompanyName = "Custom Invitations";
+            Console.WriteLine("Welcome to {0}", CompanyName);
+            Console.WriteLine("What is the name of your event?");
+            string eventName = Console.ReadLine();
+            Console.WriteLine("What is the date and time of your event? (Please format as mm dd yyyy)");
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+            var invitation1 = new Invitation(eventName, date);
+            
+            Console.Read();
+           
+        }
+    }
+}
---
+++ Additional_Features/Additional_Features/invitation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    public class Invitation
+    {
+        public string Name { get; set; }
+        public string Event { get; set; }
+        public DateTime Date { get; set; }
+        public  Invitation(string name1, string eventName, DateTime date)
+        {
+            Name = name1;
+            Event = eventName;
+            Date = date;
+            Console.WriteLine (" Dear {0}, \n Please join us for the celebration of \n {1} \n on \n {2}", name1, eventName, Convert.ToString(date));
+        }
+
+        public  Invitation(string eventName, DateTime date): this( "guest", eventName, date)
+        {
+
+        }
+    }
+}
