136263adbad4bf0484d552e612ea02cab9577775
Anastasia Reister <anastasia.reister@gmail.com>
2018-11-01 14:21:37 -0700

    find indices of array values that add up to the target value
    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    Example:
    
    Given nums = [2, 7, 11, 15], target = 9,
    
    Because nums[0] + nums[1] = 2 + 7 = 9,
    return [0, 1].

---
+++ Target_Sum/Target_Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = new int[4] { 3, 8, 11, 6 };
+            int target = 100;
+
+            int[] result = new int[2];
+            result = Solution.TwoSum(nums,  target);
+
+         
+            Console.Read();
+        }
+    }
+}
---
+++ Target_Sum/Target_Sum/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_Sum
+{
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        int [] indicesOfTarget = new int[2] { i, j };
+                        return indicesOfTarget;
+                    }
+                   
+                }
+            }
+            
+            return null;
+
+        }
+    }
+}
