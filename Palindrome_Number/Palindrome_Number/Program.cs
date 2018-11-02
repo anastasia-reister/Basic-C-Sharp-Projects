b43dfe1df00e634c9844aa45cb5f639fc55fcc14
Anastasia Reister <anastasia.reister@gmail.com>
2018-11-01 18:47:29 -0700

    Determine if number is palindrome(use stack)
    Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.

---
+++ Palindrome_Number/Palindrome_Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 123421;
+            bool isPalindrome = Solution.IsPalindrome(x);
+            Console.WriteLine(isPalindrome);
+            Console.Read();
+        }
+    }
+}
---
+++ Palindrome_Number/Palindrome_Number/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    class Solution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            Stack<string> myStack = new Stack<string>();
+            string xString = x.ToString();
+            for ( int i = 0; i<xString.Length; i++)
+            {
+                myStack.Push(xString[i].ToString());
+            }
+
+            StringBuilder reverse = new StringBuilder();
+            do { reverse.Append(myStack.Pop()); }
+            while (myStack.Count > 0); 
+           
+
+            if(xString == reverse.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
