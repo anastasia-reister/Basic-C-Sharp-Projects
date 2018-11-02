600b8bbd96f62d09002d1fd5291a4c610d3e9c94
Anastasia Reister <anastasia.reister@gmail.com>
2018-11-01 18:05:22 -0700

    Given a 32-bit signed integer, reverse digits of an integer.
    Example 1:
    
    Input: 123
    Output: 321
    Example 2:
    
    Input: -123
    Output: -321
    Example 3:
    
    Input: 120
    Output: 21
    Note:
    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.

---
+++ Reverse_Integer/Reverse_Integer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = -2147483648;
+            int z = Solution.Reverse(y);
+            Console.WriteLine(z);
+            Console.Read();
+        }
+    }
+}
---
+++ Reverse_Integer/Reverse_Integer/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    class Solution
+    {
+        public static  int Reverse(int x)
+        {
+            try
+            {
+                StringBuilder result = new StringBuilder(50);
+                if (x >= 0)
+                {
+                    string xString = x.ToString();
+                    for (int i = xString.Length - 1; i >= 0; i--)
+                    {
+                        result.Append(xString[i]);
+                    }
+                    return Convert.ToInt32(result.ToString());
+
+                }
+                else
+                {
+                    string xString = x.ToString();
+                    for (int i = xString.Length - 1; i > 0; i--)
+                    {
+                        result.Append(xString[i]);
+                    }
+
+                    int resultInt = 0 - Convert.ToInt32(result.ToString());
+                    return resultInt;
+                }
+            }
+            catch (OverflowException) { return 0; }
+            
+        }
+    }
+}
