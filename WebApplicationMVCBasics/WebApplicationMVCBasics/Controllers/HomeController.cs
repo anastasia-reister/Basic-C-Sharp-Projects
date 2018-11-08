70e592afe747f89575ad4ddc297947d47a1deda4
Anastasia Reister <anastasia.reister@gmail.com>
2018-11-08 13:44:52 -0800

    Basic MVC Web Applications

---
+++ WebApplicationMVCBasics/WebApplicationMVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVCBasics.Models;
+
+namespace WebApplicationMVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.FirstName = "Sam";
+            user.LastName = "Bell";
+            user.Id = 1;
+
+
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
